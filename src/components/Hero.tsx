95b408e8afce9d86c21d35588475acb8a9c6eaee
Kyubie <63196624+neelpatel143@users.noreply.github.com>
2026-06-29 22:54:42 +0530

    img ./

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -110,7 +110,7 @@ export function Hero() {
             >
               <div className="absolute inset-0 bg-gradient-to-t from-royal/60 to-transparent z-10" />
               <img 
-                src="img/pari.png" 
+                src="./img/pari.png" 
                 alt="Pari Patel" 
                 className="object-cover w-full h-full mix-blend-multiply opacity-90 relative z-0"
               />
